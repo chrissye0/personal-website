7f4c4d84d0f565f30db1046e314610b4d71a7010
chrissye0 <christinee1220@gmail.com>
2026-01-08 22:25:24 -0500

    changing about page css and structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,17 @@ export default function About() {
   return (
     <section className="page">
       <section className="about">
-        <h1>About Page</h1>
-        <p>My name is Chrissy!</p>
-        <p>I am a student at the Rochester Institute of Technology studying New Media Interactive Development with a minor in Web Development. I will be graduating in May 2026.</p>
-        <p>I am passionate about UI/UX design, user research, digital art, web development, and interactive technologies.</p>
-        <p>I dream of one day making the digital media that ignited my passions for art and tech in the first place, but until then, it's a work in progress!</p>
+        <section className="intro">
+          <figure className="profile">
+            <img src={'../assets/profile-pic.jpg'} alt="Profile Picture" />
+          </figure>
+          <article className="description">
+            <h1>My name is Chrissy!</h1>
+            <p>I am a student at the Rochester Institute of Technology studying New Media Interactive Development with a minor in Web Development. I will be graduating in May 2026.</p>
+            <p>I am passionate about UI/UX design, user research, digital art, web development, and interactive technologies.</p>
+            <p>I dream of one day making the digital media that ignited my passions for art and tech in the first place, but until then, it's a work in progress!</p>
+          </article>
+        </section>
       </section>
       <Footer />
     </section>
