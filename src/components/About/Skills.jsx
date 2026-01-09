e2561285cfd590c283b140fccf40bd9185844306
chrissye0 <christinee1220@gmail.com>
2026-01-08 23:51:03 -0500

    added skill pills

---
+++ src/components/About/Skills.jsx
@@ -0,0 +1,40 @@
+export default function Skills() {
+    return (
+        <section className="skills">
+            <h2>Skills</h2>
+            <section className="ui-ux-skills">
+                <h3>UI/UX</h3>
+                <button className="skill">Figma</button>
+                <button className="skill">AxureRP</button>
+                <button className="skill">UserTesting</button>
+            </section>
+            <section className="programming-skills">
+                <h3>Programming Languages & Tools</h3>
+                <button className="skill">JavaScript</button>
+                <button className="skill">TypeScript</button>
+                <button className="skill">HTML</button>
+                <button className="skill">CSS</button>
+                <button className="skill">C#</button>
+                <button className="skill">Java</button>
+                <button className="skill">React</button>
+                <button className="skill">Node.js</button>
+                <button className="skill">p5.js</button>
+                <button className="skill">Three.js</button>
+                <button className="skill">SQL</button>
+                <button className="skill">MongoDB</button>
+                <button className="skill">Unity</button>
+                <button className="skill">Git</button>
+                <button className="skill">Arduino</button>
+                <button className="skill">C++</button>
+            </section>
+            <section className="design-skills">
+                <h3>Design & Multimedia</h3>
+                <button className="skill">Adobe Illustrator</button>
+                <button className="skill">Adobe Photoshop</button>
+                <button className="skill">Adobe Premiere Pro</button>
+                <button className="skill">Blender</button>
+                <button className="skill">Procreate</button>
+            </section>
+        </section>
+    )
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,16 @@
 import Footer from '../components/Footer.jsx';
+import Intro from '../components/About/Intro.jsx';
+import Skills from '../components/About/Skills.jsx';
+import Experience from '../components/About/Experience.jsx';
 import '../styles/App.css';
 
 export default function About() {
   return (
     <section className="page">
       <section className="about">
-        <header className="intro">
-          <figure className="profile">
-            <img src={'../assets/profile-pic.jpg'} alt="Profile Picture" />
-          </figure>
-          <article className="description">
-            <h1>My name is Chrissy!</h1>
-            <p>I am a student at the Rochester Institute of Technology studying New Media Interactive Development with a minor in Web Development. I will be graduating in May 2026.</p>
-            <p>I am passionate about UI/UX design, user research, digital art, web development, and interactive technologies.</p>
-            <p>I dream of one day making the digital media that ignited my passions for art and tech in the first place, but until then, it's a work in progress!</p>
-          </article>
-        </header>
+        <Intro />
+        <Skills />
+        <Experience />
       </section>
       <Footer />
     </section>
