6881cd103065dc0a7610bc760d604adc392aa621
chrissye0 <christinee1220@gmail.com>
2026-01-11 15:47:07 -0500

    added project cards

--- src/components/Home/Project.jsx
+++ src/components/Home/Project.jsx
@@ -1,10 +1,35 @@
-export default function Project({ title, description, link }) {
+export default function Project({ title, description, link, image }) {
   return (
-    <a href={link}>
-        <article className="project">
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </article>
-    </a>
+    /** project card component */
+    <>
+      <section className="project">
+        <img src={image} alt={title} className="project-image" />
+
+        <section className="project-info">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </section>
+
+        {/* Overlay link makes entire card clickable */}
+        <a
+          href={link}
+          className="project-link"
+          aria-label={`View ${title}`}
+        />
+      </section>
+
+      {/* <div className="card">
+        <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/greece.jpg" />
+        <div class="card-trip-infos">
+          <div>
+            <h2>Title here</h2>
+            <p>Short description here!</p>
+          </div>
+          <h2 class="card-trip-pricing">£99.99</h2>
+          <img src="https://kitt.lewagon.com/placeholder/users/dmbf29" class="card-trip-user avatar-bordered" />
+        </div>
+        <a href="#" class='card-link'></a>
+      </div> */}
+    </>
   )
 }
--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -1,16 +1,65 @@
 import Project from './Project.jsx';
+import flossBossImage from '../../assets/floss-boss.png';
+import msgRedesignImage from '../../assets/msg-redesign.jpg';
+import yearbookImage from '../../assets/yearbook.png';
+import alumniPagesImage from '../../assets/alumni-pages.png';
+
+const projects = [
+  {
+    title: "Floss Boss",
+    description: "An alternative interface interactive experience.",
+    image: flossBossImage,
+    link: "/floss-boss",
+  },
+  {
+    title: "MSG.com Redesign",
+    description: "A redesign of Madison Square Garden's homepage I did during my internship.",
+    image: msgRedesignImage,
+    link: "/msg-redesign",
+  },
+  {
+    title: "Yearbook",
+    description: "A year-long project where I led a small team to create a yearbook for RIT's Computer Science House.",
+    image: yearbookImage,
+    link: "/yearbook",
+  },
+  {
+    title: "Knicks & Rangers Alumni Pages",
+    description: "Webpage designs for retired New York Knicks and New York Rangers players.",
+    image: alumniPagesImage,
+    link: "/alumni-pages",
+  },
+  // {
+  //   title: "Portfolio Website",
+  //   description: "How I made this website!",
+  //   image: "/images/portfolio-website.jpg",
+  //   link: "/portfolio",
+  // },
+];
+
 
 export default function Projects() {
   return (
     <>
       <h2>Projects</h2>
       <section className="projects">
+        {projects.map((project) => (
+          <Project
+            key={project.title}
+            image={project.image}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
+      </section>
+      {/* <section className="projects">
         <Project title="Floss Boss" description="An alternative interface interactive experience." link="/floss-boss" />
         <Project title="MSG.com Redesign" description="A redesign of Madison Square Garden's homepage I did during my internship." link="/msg-redesign" />
         <Project title="Yearbook" description="A year-long project where I led a small team to create a yearbook for RIT's Computer Science House." link="/yearbook" />
         <Project title="Portfolio Website" description="How I made this website!" link="/portfolio" />
         <Project title="Knicks & Rangers Alumni Pages" description="Webpage designs for retired New York Knicks and New York Rangers players." link="/alumni-pages" />
-      </section>
+      </section> */}
     </>
   )
 }
