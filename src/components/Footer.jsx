42c193bcd6dd14bd461d4e5989e927d9c9a4cbcd
chrissye0 <christinee1220@gmail.com>
2026-01-14 15:24:54 -0500

    css changes to nav, footer, and header tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,11 @@ import emailIcon from '../assets/email.svg';
 export default function Footer() {
   return (
     <footer>
-      <p> Thanks for visiting my website! If you want to reach out, find me on LinkedIn or send over an email! :)</p>
-      <section className="footer-links">
+      <section className="footer-left">
+        <h1> Thanks for visiting!</h1>
+        <h2>Let's chat :)</h2>
+      </section>
+      <section className="footer-right" >
         <section className="footer-link">
           <img src={linkedinIcon} alt="LinkedIn Icon" />
           <a href="https://www.linkedin.com/in/christine-espeleta/" target="_blank" rel="noopener noreferrer">christine-espeleta</a>
@@ -13,6 +16,11 @@ export default function Footer() {
         <section className="footer-link">
           <img src={emailIcon} alt="Email Icon" />
           <a href="mailto:christinee1220@gmail.com" target="_blank" rel="noopener noreferrer">christinee1220@gmail.com</a>
+        </section>
+        <br />
+        <section className="footer-credit">
+          <a href="https://github.com/chrissye0/personal-website">made with sweet treats & love</a>
+          <p>© Christine Espeleta 2026</p>
         </section>
       </section>
     </footer>
