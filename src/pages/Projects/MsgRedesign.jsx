7335dbaf799f404b3d8b957b1544bebad4c9857f
chrissye0 <christinee1220@gmail.com>
2026-01-19 14:13:53 -0500

    added the rest of the images :))

--- src/pages/Projects/MsgRedesign.jsx
+++ src/pages/Projects/MsgRedesign.jsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/Footer.jsx";
 import Sidebar from "../../components/Sidebar.jsx";
+import msgComputerPrototype from "../../assets/msg-computer-prototype.png";
 import msgProcessImage from "../../assets/msg-redesign-process.png";
 import msgPrototypeV1 from "../../assets/1440-v1.png";
 import msgPrototypeV2 from "../../assets/1440-v2.png";
@@ -37,7 +38,11 @@ export default function MsgRedesign() {
               </section>
             </section>
           </header>
-          {/** prototype image */}
+          <br />
+          <section className="project-page-image">
+            <img src={msgComputerPrototype} alt="msg prototype image" />
+          </section>
+          <br />
           <article className="msg-problem" id="Problem">
             <h2>Problem</h2>
             <h3>MSG.com users believe there's an imbalance of event and venue information and show little click interactivity on the homepage.</h3>
@@ -57,6 +62,7 @@ export default function MsgRedesign() {
           </article>
           <article className="msg-process" id="Process">
             <h2>Process</h2>
+            <br />
             <img src={msgProcessImage} alt="process image" width={"50%"} height={"auto"} />
           </article>
           <article className="competitive-analysis" id="Competitive Analysis">
@@ -76,11 +82,14 @@ export default function MsgRedesign() {
             <h2>A/B Testing</h2>
             <p>After some design feedback from my team, I settled on two mockup designs I was confident in. The differences between the two included the “Plan Your Visit” section and the event carousel at the top. In the first version, “Plan Your Visit” is at the bottom. The user can click through photos of the arena, and to the right, the user can read general venue information and navigate to their respective pages. The version on the right contains the icons and boxes from before and is instead higher up on the homepage. For the carousel, the bar at the bottom with the navigation arrows is different, with one being an extension of the arena background and the other having a dark blue background.</p>
             <p>At this point, I conducted an A/B test asking users to scroll through both prototypes individually, express their thoughts, note any differences between the two, and then see them both side-by-side. Results from the test showed people preferred “Plan Your Visit” being at the top, as they would have to scroll down past all the events in the other version. The icons also made the design more readable and accessible for users. There weren’t many strong opinions regarding the carousel, but some cited that the dark blue bar made the design less busy and more “modern.” I went forward with the 2nd mockup by replacing placeholders with imagery and event content and finally conducted another A/B test comparing this mockup and the current MSG.com website.</p>
+            <br />
             <section className="msg-prototypes-AB">
               <img src={msgPrototypeV5A} alt="prototype 5A" />
               <img src={msgPrototypeV5B} alt="prototype 5B" />
             </section>
+            <br />
             <p>The results from the 2nd A/B test showed that most tested users preferred my prototype to the official website. They liked the use of icons and imagery and how more events were displayed and organized. People could more easily find an event they liked directly from the homepage and icons pointed toward a more user-friendly and accessible design. Users also had a positive reception toward the social media integration at the bottom. Points of feedback included having more arena imagery and creating more visual unity with the event photo and description in the top carousel.</p>
+            <br />
             <section className="msg-prototypes-AB">
               <img src={msgPrototypeV6A} alt="prototype 6A" />
               <img src={msgPrototypeV6B} alt="prototype 6B" />
@@ -107,15 +116,6 @@ export default function MsgRedesign() {
               </section>
             </section>
           </article>
-          {/* <article className="msg-competitive-analysis" id="Competitive Analysis">
-            <h2>Competitive Analysis</h2>
-            <p>My first batch of user tests was for a competitive analysis, in which I compared the MSG.com homepage with that of competitor venue homepages, more specifically Barclays Center, Climate Pledge Arena, T-Mobile Arena, and Crypto.com Arena. I had screening questions (such that the test targeted specifically venue ticket buyers) and had users rate the homepages based on their design, navigation, and ease of use. I also collected the Net Promoter Scores of each site and general user feedback. I evaluated the strengths and weaknesses of each venue and compared them to those of the MSG.com homepage and presented findings to the Digital Experience team as well as some from the Sports and Marketing teams. I then created my first few design mockups of the homepage, as seen below.</p>
-          </article>
-          <article className="msg-process" id="Process">
-            <h2>Process</h2>
-            <p>After some design feedback from my team, I settled on two mockup designs I was confident in. The differences between the two included the “Plan Your Visit” section and the event carousel at the top. In the first version, Plan Your Visit is at the bottom. The user can click through photos of the arena, and to the right, the user can read general venue information and navigate to their respective pages. The version on the right contains the icons and boxes from before and is instead higher up on the homepage. For the carousel, the bar at the bottom with the navigation arrows is different, with one being an extension of the arena background and the other having a dark blue background.</p>
-            <p>At this point, I conducted an A/B test asking users to scroll through both prototypes individually, express their thoughts, note any differences between the two, and then see them both side-by-side. Results from the test showed people preferred “Plan Your Visit” being at the top, as they would have to scroll down past all the events in the other version. The icons also made the design more readable and accessible for users. There weren’t many strong opinions regarding the carousel, but some cited that the dark blue bar made the design less busy and more “modern.” I went forward with the 2nd mockup by replacing placeholders with imagery and event content and finally conducted another A/B test comparing this mockup and the current MSG.com website.</p>
-          </article> */}
         </section>
       </section>
       <Footer />
--- src/pages/Projects/Yearbook.jsx
+++ src/pages/Projects/Yearbook.jsx
@@ -1,3 +1,26 @@
+import yearbookImage from "../../assets/yearbook.png";
+import yearbookProcessImage from "../../assets/yearbook-process.png";
+import oldRITYearbook1 from "../../assets/old-rit-yearbook-1.jpeg";
+import oldRITYearbook2 from "../../assets/old-rit-yearbook-2.jpeg";
+import oldRITYearbook3 from "../../assets/old-rit-yearbook-3.jpeg";
+import oldRITYearbook4 from "../../assets/old-rit-yearbook-4.jpeg";
+import oldCSHYearbook1 from "../../assets/old-csh-yearbook-1.png";
+import oldCSHYearbook2 from "../../assets/old-csh-yearbook-2.png";
+import oldCSHYearbook3 from "../../assets/old-csh-yearbook-3.png";
+import oldCSHYearbook4 from "../../assets/old-csh-yearbook-4.png";
+import yearbookProcess1 from "../../assets/yearbook-process-1.png";
+import yearbookProcess2 from "../../assets/yearbook-process-2.png";
+import yearbookProcess3 from "../../assets/yearbook-process-3.png";
+import yearbookProcess4 from "../../assets/yearbook-process-4.png";
+import yearbookProcess5 from "../../assets/yearbook-process-5.png";
+import yearbookProcess6 from "../../assets/yearbook-process-6.png";
+import yearbookProcess7 from "../../assets/yearbook-process-7.png";
+import yearbookProcess8 from "../../assets/yearbook-process-8.png";
+import yearbookResults1 from "../../assets/yearbook-results-1.jpg";
+import yearbookResults2 from "../../assets/yearbook-results-2.jpg";
+import yearbookResults3 from "../../assets/yearbook-results-3.jpg";
+import yearbookResults4 from "../../assets/yearbook-results-4.jpg";
+
 import Footer from "../../components/Footer.jsx";
 import Sidebar from "../../components/Sidebar.jsx";
 
@@ -10,16 +33,34 @@ export default function Yearbook() {
           <header className="project-header">
             <h1>CSH Yearbook</h1>
             <p>A multi-year creative team project I led for RIT's Computer Science House.</p>
-            <section className="project-page-tags">
-              <button className="project-skill">Product Management</button>
-              <button className="project-skill">Adobe Illustrator</button>
-              <button className="project-skill">Adobe Photoshop</button>
-              <button className="project-skill">Canva</button>
-              <button className="project-skill">Blender</button>
-              <button className="project-skill">Notion</button>
-              <button className="project-skill">Trello</button>
+            <header className="project-mini-headers">
+              <section className="role">
+                <h4>Role</h4>
+                <h3>Director</h3>
+              </section>
+              <section className="timeline">
+                <h4>Timeline</h4>
+                <h3>Sept 2022 - May 2023, Sept 2023 - May 2024, Sept 2024 - May 2025</h3>
+              </section>
+              <section className="tools">
+                <h4>Tools</h4>
+                <section className="project-page-tags">
+                  <button className="project-skill">Product Management</button>
+                  <button className="project-skill">Adobe Illustrator</button>
+                  <button className="project-skill">Adobe Photoshop</button>
+                  <button className="project-skill">Canva</button>
+                  <button className="project-skill">Blender</button>
+                  <button className="project-skill">Notion</button>
+                  <button className="project-skill">Trello</button>
+                </section>
+              </section>
+            </header>
+            <br />
+            <section className="project-page-image">
+              <img src={yearbookImage} alt="yearbook image" />
             </section>
           </header>
+          <br />
           <article className="yearbook-intro" id="Background">
             <h2>Background</h2>
             <p>This project began in the Fall Semester of my freshman year, and I have led it ever since. I was heavily involved in the student organization Computer Science House and I soon learned of a yearbook project that died down as a result of COVID. This club meant a lot to me throughout my college experience, and with my prior design experience, I thought this project was a perfect way to give back to the community that gave so much to me.
@@ -28,20 +69,52 @@ export default function Yearbook() {
           </article>
           <article className="yearbook-process" id="Process">
             <h2>Process</h2>
-            
+            <img src={yearbookProcessImage} alt="process image" width={"50%"} height={"auto"} />
           </article>
           <article className="yearbook-research" id="Research">
             <h2>Research</h2>
             <p>Before beginning the design process, we conducted research on previous CSH yearbooks and feedback from members. In 2022-2023, we referenced projects pre-COVID, yet in the projects after, we could directly utilize the feedback we received from the year before. Additionally, we reviewed select RIT yearbooks from the RIT Archive, which contained student-produced yearbooks from 1912 to 1983. We drew inspiration for club branding, imagery, different layouts, design consistency, and more.</p>
+            <br />
+            <section className="old-rit-yearbooks">
+              <img src={oldRITYearbook1} alt="old RIT yearbook image" />
+              <img src={oldRITYearbook2} alt="old RIT yearbook image" />
+              <img src={oldRITYearbook3} alt="old RIT yearbook image" />
+              <img src={oldRITYearbook4} alt="old RIT yearbook image" />
+            </section>
+            <br />
+            <section className="old-csh-yearbooks">
+              <img src={oldCSHYearbook1} alt="old CSH yearbook image" />
+              <img src={oldCSHYearbook2} alt="old CSH yearbook image" />
+              <img src={oldCSHYearbook3} alt="old CSH yearbook image" />
+              <img src={oldCSHYearbook4} alt="old CSH yearbook image" />
+            </section>
           </article>
           <article className="yearbook-design" id="Ideation, Design, & Feedback">
             <h2>Ideation, Design, & Feedback</h2>
             <p>As the director, I held weekly meetings and check-ins to track progress, feedback, and our timeline. We stored information in a Trello board and a series of Google Sheets and recorded notes through Notion. I assigned people roles and responsibilities for sections and provided adequate design guidelines and templates to ensure team members felt well-prepared. A key point I wanted to address was that anyone, regardless of their experience with design, could contribute, so we used Canva as it was both free and easy to learn.</p>
             <p>I designed the general page template as well as the cover, and I was also responsible for managing logistics and ensuring we had the correct information for each member. We iterated through different page designs and layouts based on feedback both from the team and the rest of CSH, which also led to yearly design themes and sections specific to the year. Depending on the yearbook's theme, we used several additional design tools. We created vector-based designs with Adobe Illustrator and Figma in 2023-2024 and 3D vaporwave designs with Blender in 2024-2025. I oversaw the process and gave members feedback on pages to maintain visual coherence and consistency. I used Adobe Photoshop for photo editing as well.</p>
+            <br />
+            <section className="yearbook-process-images">
+              <img src={yearbookProcess1} alt="yearbook process image" />
+              <img src={yearbookProcess2} alt="yearbook process image" />
+              <img src={yearbookProcess3} alt="yearbook process image" />
+              <img src={yearbookProcess4} alt="yearbook process image" />
+              <img src={yearbookProcess5} alt="yearbook process image" />
+              <img src={yearbookProcess6} alt="yearbook process image" />
+              <img src={yearbookProcess7} alt="yearbook process image" />
+              <img src={yearbookProcess8} alt="yearbook process image" />
+            </section>
           </article>
           <article className="yearbook-results" id="Results">
             <h2>Results</h2>
             <p>With the 2022-2023, 2023-2024, and 2024-2025 yearbooks, we produced and sold 150+ yearbooks to CSH members and alumni. The yearbooks were met with positive reception and now serve as a memorable keepsake for CSH members to look back on their time in the community.</p>
+            <br />
+            <section className="yearbook-results-images">
+              <img src={yearbookResults1} alt="yearbook results image" />
+              <img src={yearbookResults2} alt="yearbook results image" />
+              <img src={yearbookResults3} alt="yearbook results image" />
+              <img src={yearbookResults4} alt="yearbook results image" />
+            </section>
           </article>
         </section>
       </section>
